f7a86c8c699e5b74dfa022f0efecbc04d514a090
roman.strazanec <strazanec.roman@gmail.com>
2022-09-14 16:50:51 +0200

    Section 12 Authentication in Microfrontends 112 Allowing Signout

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -19,7 +19,7 @@ export default () => {
         <BrowserRouter>
             <StylesProvider generateClassName={generateClassName}>
                 <div>
-                    <Header isSignedIn={isSignedIn}/>
+                    <Header onSignOut={() => setIsSignedIn(false)} isSignedIn={isSignedIn} />
                     <Suspense fallback={<Progress />}>
                       <Switch>
                         <Route path="/auth">
